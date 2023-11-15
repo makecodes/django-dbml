f6be0e8d62b624c3c81b4ec5b4d3af2e8b23794a
Michel Wilhelm <mwilhelm@collective.com>
2023-11-15 12:46:54 -0300

    chore(publish-to-test-pypi.yml): remove unused WITH_V environment variable to simplify workflow configuration fix(update_version.py): remove unnecessary replace function call to improve version formatting

--- update_version.py
+++ update_version.py
@@ -10,7 +10,7 @@ def get_git_tag():
 def update_version_in_setup_cfg(version):
     config = configparser.ConfigParser()
     config.read("setup.cfg")
-    version = version.lstrip("v").replace("-", ".")
+    version = version.lstrip("v")
     config["metadata"]["version"] = version
     with open("setup.cfg", "w") as configfile:
         config.write(configfile)
